906f4d9874a54d2b88938e2f9afc949fd305ac97
abdalnser47-web <abd.alnser47@gmail.com>
2026-04-13 04:05:34 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const cacheName = 'alzein-v1.4';
+const cacheName = 'alzein-v1.5'; // غير الرقم مع كل تحديث
 const assets = ['./', './index.html'];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+    // إجبار النسخة الجديدة على التنشيط فوراً
+    self.skipWaiting(); 
+    e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (e) => {
+    // حذف الكاش القديم تماماً
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)));
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
